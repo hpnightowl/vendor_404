e8e895cacc917cf936684c65de231c8a27ddb5d0
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:34:39 +0530

    vendor: Bring in QTI privapp and whitelist permissions
    Change-Id: If8fd3adda8fd062f215d05873dd6cf21f74f88a5

--- configs/common.mk
+++ configs/common.mk
@@ -32,3 +32,8 @@ DISABLE_EAP_PROXY := true
 
 # Move Wi-Fi modules to vendor
 PRODUCT_VENDOR_MOVE_ENABLED := true
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/404/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/404/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
